2f096723aa7676d0c399eb862480463acafe3df6
Marc Rene Arns <arns@stridor.net>
2013-01-28 21:56:38 +0100

    cleanup

--- examples/example.go
+++ examples/example.go
@@ -5,10 +5,6 @@ import (
 	"github.com/metakeule/dispatch"
 )
 
-func p(i interface{}) {
-	fmt.Println(i)
-}
-
 type I int
 
 func main() {
@@ -16,7 +12,7 @@ func main() {
 	// setup the dispatcher
 	d := dispatch.New()
 
-	// we have fallback functions for unhandled types
+	// we may have fallback functions for unhandled types
 	fallback := func(in interface{}, out interface{}) (didHandle bool, err error) {
 		didHandle = true
 		fmt.Printf("fallback for %#v\n", in)
@@ -29,19 +25,20 @@ func main() {
 	// they have to cast to the type they serve, but they don't
 	// need to check for type casting of the interface.
 	// they should however return an error for other situations
-	d.SetHandler("", func(in interface{}, out interface{}) (err error) {
-		fmt.Printf("%s is a string\n", in.(string))
-		return
-	})
+	d.SetHandler("",
+		func(in interface{}, out interface{}) (err error) {
+			fmt.Printf("%s is a string\n", in.(string))
+			return
+		})
 
-	d.SetHandler(I(0), func(in interface{}, out interface{}) (err error) {
-		fmt.Printf("%d is a I\n", in.(I))
-		return
-	})
+	d.SetHandler(I(0),
+		func(in interface{}, out interface{}) (err error) {
+			fmt.Printf("%d is a I\n", in.(I))
+			return
+		})
 
 	// let the fun begin!
-	d.Dispatch("my string", "")       // my string is a string
-	d.Dispatch(I(3), "")              // 3 is a I
-	fmt.Println(d.Dispatch(34.0, "")) // Error: type float64 is not registered, use AddType()
-	d.Dispatch(34, "")                // fallback for 34
+	d.Dispatch("my string", "") // my string is a string
+	d.Dispatch(I(3), "")        // 3 is a I
+	d.Dispatch(34, "")          // fallback for 34
 }
